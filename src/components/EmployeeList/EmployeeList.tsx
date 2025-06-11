d87eced4d8a5db96d45e792361da12ced47c774b
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-06-12 02:32:59 +1000

    adds search bar

--- src/components/EmployeeList/EmployeeList.tsx
+++ src/components/EmployeeList/EmployeeList.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { useDataContext } from "../../context/DataContextProvider";
 import EmployeeCard from "../Employee/EmployeeCard";
-import style from './EmployeeList.module.scss'
+import style from "./EmployeeList.module.scss";
 
 const EmployeeList = () => {
+  const [search, setSearch] = useState("");
   const { employees } = useDataContext();
 
-if(employees.length === 0) {
-  return <div>There are no employees yet!</div>
-}
+  console.log(search);
+
+  if (employees.length === 0) {
+    return <div>There are no employees yet!</div>;
+  }
 
   return (
-    <div className={style.container}>
-      {employees.map((employee) => (
-        <EmployeeCard key={employee.id} data={employee} />
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Search for an employee"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className={style.container}>
+        {employees
+          .filter((employee) => {
+            return search.toLowerCase() === ""
+              ? employee
+              : employee.firstName.toLowerCase().includes(search);
+          })
+          .map((employee) => (
+            <EmployeeCard key={employee.id} data={employee} />
+          ))}
+      </div>
     </div>
   );
 };
-
 
 export default EmployeeList;
