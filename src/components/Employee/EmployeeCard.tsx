f9d8846cb48bc48307691f192540fb3029cabbd2
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-07-02 13:28:28 +1000

    adds delete confirmation modal & button

--- src/components/Employee/EmployeeCard.tsx
+++ src/components/Employee/EmployeeCard.tsx
@@ -5,6 +5,7 @@ import {
 } from "../../context/DataContextProvider";
 import { deleteEmployeeById } from "../../services/dataServices";
 import styles from "./EmployeeCard.module.scss";
+import { useState } from "react";
 
 type EmployeeProps = {
   data: Employee;
@@ -12,8 +13,13 @@ type EmployeeProps = {
 
 const EmployeeCard = ({ data }: EmployeeProps) => {
   const navigate = useNavigate();
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
   const { setEmployees } = useDataContext();
+
+  const handleConfirmDelete = () => {
+    setConfirmDelete(true);
+  };
 
   const handleRemove = async () => {
     deleteEmployeeById(data.id);
@@ -27,21 +33,37 @@ const EmployeeCard = ({ data }: EmployeeProps) => {
   };
 
   return (
-    <div className={styles.container}>
-      <p>
-        {data.firstName} {data?.middlename} {data.lastName}
-      </p>
-      <img
-        src={data.profileImageUrl ?? "https://employeetrackerpb.s3.ap-southeast-2.amazonaws.com/public/usericon.JPG"}
-        alt="user"
-      />
-      <p>{data.contractType}</p>
-      <p>{data.email}</p>
-      <div>
-        <button onClick={() => handleEdit()}>Edit</button>
-        <button onClick={() => handleRemove()}>Remove</button>
+    <>
+      {confirmDelete && (
+        <div className={styles.modalOverlay}>
+          <div className={styles.modal}>
+            <p>Are you sure you want to delete {data.firstName} {data.lastName}?</p>
+            <div className={styles.modalButtons}>
+              <button onClick={handleRemove}>Yes, delete</button>
+              <button onClick={() => setConfirmDelete(false)}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className={styles.container}>
+        <p>
+          {data.firstName} {data?.middlename} {data.lastName}
+        </p>
+        <img
+          src={
+            data.profileImageUrl ??
+            "https://employeetrackerpb.s3.ap-southeast-2.amazonaws.com/public/usericon.JPG"
+          }
+          alt="user"
+        />
+        <p>{data.contractType}</p>
+        <p>{data.email}</p>
+        <div>
+          <button onClick={() => handleEdit()}>Edit</button>
+          <button onClick={() => handleConfirmDelete()}>Remove</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
